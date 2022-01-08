ac046f9318e161933ca69fd469667d58e64aee6f
bladshed <francismarkserrano@yahoo.com>
2022-01-08 21:03:01 +0800

    created outfits put api

--- index.js
+++ index.js
@@ -24,56 +24,22 @@ async function main() {
     // ROUTES
     app.get('/', function(req,res){
         res.json({
-            "message":"Hello from my API endpoint"
+            "message":"Welcome to my Project 2 APIs"
         })
-    })
-
-    // Route to add in a new food sighting
-    app.post('/food_sighting', async function(req,res){
-        const db = MongoUtil.getDB();
-        
-        // define the sample document:
-        /*
-        {
-            description: "a description about the food sighting",
-            foods: ["roti prata", "fishball", "fried rice"],
-            date: "2021-12-31"
-        }
-        */
-
-        let description = req.body.description;
-        let foods = req.body.foods;
-        let date = new Date(req.body.date) || new Date();
-
-        // if any lines in the try block causes an error, immediately go to the catch part
-        try {
-            let result = await db.collection('sightings').insertOne({
-                'description': description,
-                'foods': foods,
-                'date': date
-            })
-            res.json(result);
-        }
-        catch (e) {
-            // if there is an error while trying to add to the database,
-            // we will inform the client
-            res.status(500); // tell the client we had an internal server error
-            res.json({
-                'error': "Failed to add record"
-            })
-        }
     })
 
     // get all outfit records
     app.get('/outfits', async function(req,res){
+        // get instance of Mongo db
         const db = MongoUtil.getDB();
+
         try {
-            // let results = await db.collection('sightings').find(critera).toArray();
             let results = await db.collection('outfits').find({},{
                 'projection': {
                     'submittedBy': 1,
                     'type': 1,
-                    'gender': 1
+                    'gender': 1,
+                    'description' : 1
                 }
             }).toArray();
 
@@ -86,6 +52,72 @@ async function main() {
                 'error':"Unable to fetch records"
             })
         }        
+    })
+
+    // add outfit record
+    app.post('/outfits', async function(req,res){
+        // get instance of Mongo db
+        const db = MongoUtil.getDB();
+
+        // initialize variables
+        let submittedBy = req.body.submittedBy;
+        let type = req.body.type;
+        let gender = req.body.gender;
+        let img_url = req.body.img_url;
+        let description = req.body.description;
+        let dateCreated = new Date();
+
+        // if any lines in the try block causes an error, immediately go to the catch part
+        try {
+            let result = await db.collection('outfits').insertOne({
+                'submittedBy': submittedBy,
+                'type': type,
+                'gender': gender,
+                'img_url': img_url,
+                'description' : description,
+                'dateCreated': dateCreated
+            })
+            res.json(result);
+        }
+        catch (e) {
+            // if there is an error while trying to add to the database,
+            // we will inform the client
+            res.status(500); // tell the client we had an internal server error
+            res.json({
+                'message': "Failed to add record",
+                'error': e.message
+            })
+        }
+    })
+
+    // modify outfit record
+    app.put('/outfits/:id', async function(req,res){
+        // get instance of Mongo db
+        const db = MongoUtil.getDB();
+        
+        try {
+            await db.collection('outfits').updateOne({
+                '_id':ObjectId(req.params.id)
+            }, {
+                '$set': {
+                    'submittedBy': req.body.submittedBy,
+                    'type': req.body.type,
+                    'gender': req.body.gender,
+                    'img_url': req.body.img_url,
+                    'description' : req.body.description,
+                    'dateModified': new Date()
+                }
+            })
+            res.json({
+                'message':"success"
+            })
+        } catch(e) {
+            res.status(500);
+            res.json({
+                'message':"Unable to update document",
+                'error': e.message
+            })
+        }
     })
 
     // we are using PUT instead of POST because we are updating an existing document by providing a replacement document
